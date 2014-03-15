ca7b0b5d8c5ea18ddea512fde5928707b0e5ec94
Luke Heavens <luke.heavens@gmail.com>
2014-03-15 23:23:03 +0000

    forgot to add files

---
+++ src/projectFiles/DatabaseServlet.java
@@ -0,0 +1,50 @@
+package projectFiles;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DatabaseServlet
+ */
+public class DatabaseServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DatabaseServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	 	try {
+	 		//Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			String DB="jdbc:mysql://stusql.dcs.shef.ac.uk/stucat039?user=aca11lh&password=lhsheff1!";
+			Connection conn = DriverManager.getConnection(DB);
+			conn.close();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException  | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
