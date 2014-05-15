fed8e2c6490a9abac1c7f4b2206e7e15241c0a2d
Luke Heavens <luke.heavens@gmail.com>
2014-05-15 21:31:47 +0100

    file jiggling

--- src/projectFiles/RDFConnector.java
+++ src/projectFiles/RDFConnector.java
@@ -14,46 +14,6 @@ public class RDFConnector {
 		
 	}
 
-	public HashMap<String, String> showUser(String username) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public List<HashMap<String, String>> getRetweetersOfUser(long userId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public List<HashMap<String, String>> getUserRetweets(long userId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public List<HashMap<String, String>> getUserLocations(long userId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public List<HashMap<String, String>> getUserKeywords(long userId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void closeConnection() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public HashMap<String, String> showVenue(String venueName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public List<HashMap<String, String>> getVenueVisitors(String venueId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	public void addUsers(User user) {
 		// TODO Auto-generated method stub
 		
@@ -88,5 +48,44 @@ public class RDFConnector {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public HashMap<String, String> showUser(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	public List<HashMap<String, String>> getRetweetersOfUser(long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<HashMap<String, String>> getUserRetweets(long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<HashMap<String, String>> getUserLocations(long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<HashMap<String, String>> getUserKeywords(long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public HashMap<String, String> showVenue(String venueName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<HashMap<String, String>> getVenueVisitors(String venueId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void closeConnection() {
+		// TODO Auto-generated method stub
+		
+	}
 }
